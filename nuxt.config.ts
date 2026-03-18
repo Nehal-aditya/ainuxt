42bdfc9d1de1b174a4e8657560a87c9711b97677
Nehal-aditya <nehaladityaresearch@outlook.com>
2026-03-18 21:12:36 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
-// nuxt.config.ts
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {
-    enabled: true
-  },
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
@@ -14,25 +10,43 @@ export default defineNuxtConfig({
     'nuxt-csurf'
   ],
 
+  devtools: {
+    enabled: true
+  },
+
   css: ['~/assets/css/main.css'],
 
   mdc: {
-    headings: { anchorLinks: false },
-    highlight: { shikiEngine: 'javascript' }
+    headings: {
+      anchorLinks: false
+    },
+    highlight: {
+      // noApiRoute: true
+      shikiEngine: 'javascript'
+    }
   },
 
-  experimental: { viewTransition: true },
+  experimental: {
+    viewTransition: true
+  },
 
   compatibilityDate: '2024-07-11',
 
   nitro: {
-    experimental: { openAPI: true }
+    experimental: {
+      openAPI: true
+    }
   },
 
-  hub: { db: 'sqlite', blob: true },
+  hub: {
+    db: 'sqlite',
+    blob: true
+  },
 
   vite: {
-    optimizeDeps: { include: ['striptags'] }
+    optimizeDeps: {
+      include: ['striptags']
+    }
   },
 
   eslint: {
@@ -44,8 +58,8 @@ export default defineNuxtConfig({
     }
   },
 
-  //Add this for GitHub Pages
+  // 👇 Required for GitHub Pages
   app: {
-    baseURL: '/ainuxt/'   // replace with your repo name
+    baseURL: '/ainuxt/' // replace with your repo name
   }
 })
